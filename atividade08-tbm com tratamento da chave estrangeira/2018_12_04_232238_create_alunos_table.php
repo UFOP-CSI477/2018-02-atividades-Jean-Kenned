75e1f5a1bedd165167ef38e4cf0aca7cd09547d6
Jean Kenned <jeanjk99@hotmail.com>
2018-12-08 02:12:38 -0200

    Add files via upload

---
+++ atividade08-tbm com tratamento da chave estrangeira/2018_12_04_232238_create_alunos_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlunosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alunos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('matricula',90);
+            $table->string('email');
+            $table->integer('cidade_id')->unsigned();
+            $table->timestamps();
+
+        });
+
+        Schema::table('alunos', function (Blueprint $table) {
+
+            $table->foreign('cidade_id')->references('id')-> on ('cidades')->onDelete('cascade');
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alunos');
+    }
+}
---
+++ atividade08-tbm com tratamento da chave estrangeira/AlunoController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Aluno;
+use App\Cidade;
+
+use Illuminate\Http\Request;
+
+class AlunoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $alunos = Aluno::all();
+        return view('alunos.index') -> with('alunos', $alunos);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $cidades = Cidade::all();
+        return view('alunos.create')->with("cidades",$cidades);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+      Aluno::create($request->all());
+      return redirect()->route('alunos.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Aluno $aluno)
+    {
+        return view('alunos.show')->with('aluno',$aluno);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Aluno $aluno)
+    {
+      $cidades = Cidade::orderBy('nome')->get();
+      return view('alunos.edit')->with('cidades',$cidades)->with('aluno',$aluno);
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Aluno $aluno)
+    {
+      $aluno->fill($request->all());
+      $aluno->save();
+
+      session()->flash('mensagem','Aluno atualizado com sucesso');
+
+      return redirect()->route('alunos.index');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Aluno $aluno)
+    {
+      //faz aqui validacao de chave estrangeira por exemplo
+
+      //Excluir
+      $aluno -> delete();
+      session()->flash('mensagem',"Aluno excluído com sucesso");
+      return redirect()->route('alunos.index');
+    }
+}
