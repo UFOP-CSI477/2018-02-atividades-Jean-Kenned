7be6313bdd289e59a6368c7417e42bd568672586
Jean Kenned <jeanjk99@hotmail.com>
2018-12-08 02:16:54 -0200

    Add files via upload

---
+++ atividade08-tbm com tratamento da chave estrangeira/cidades/show.blade.php
@@ -0,0 +1,23 @@
+@extends('layout')
+
+@section('conteudo')
+
+<!--aqui usa sintaxe blade-->
+  <h1>Cidade:  {{$cidade->nome}}</h1>
+  <p>Código: {{$cidade->id}}</p>
+  <p>Estado:{{$cidade->estado->nome}}-{{$cidade->estado->sigla}}</p>
+
+  <!--<a href="/estados">Voltar</a> ou -->
+  <a href="{{route('cidades.index')}}">Voltar</a>
+  <a href="{{route('cidades.edit',$cidade->id)}}">Editar</a>
+
+
+  <form  action="{{route('cidades.destroy',$cidade->id)}}" method="post" onsubmit="return confirm('Confirma exclusão da Cidade: {{$cidade->nome}}?');">
+
+    @csrf
+    @method('DELETE')
+    <input type="submit" value="Excluir">
+
+  </form>
+
+@endsection
